f01cd7f12907f3766cf470493be7641147dc6187
siddharth-kothari-spk <siddharth.kothari.spk>
2023-06-10 22:54:12 +0530

    Initial Commit

---
+++ Camera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by Siddharth Kothari on 10/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
