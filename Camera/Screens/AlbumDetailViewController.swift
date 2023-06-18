7ac3ac4c0306f6799ec7fbcc25cfb020775e1b1a
siddharth-kothari-spk <siddharth.kothari.spk@gmail.com>
2023-06-18 09:14:16 +0530

    controller to show album images

--- Camera/Screens/AlbumDetailViewController.swift
+++ Camera/Screens/AlbumDetailViewController.swift
@@ -6,24 +6,45 @@
 //
 
 import UIKit
+import CoreData
 
-class AlbumDetailViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class AlbumDetailViewController: UIViewController, Storyboarded {
+    weak var coordinator : MainCoordinator?
+    private var collectionView: UICollectionView!
+    private var album: Album
+    
+    init(album: Album) {
+        self.album = album
+        super.init(nibName: nil, bundle: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Album Detail"
+        setupCollectionView()
+    }
+    
+    private func setupCollectionView() {
+        collectionView = Helpers.getCollectionView(view: view, delegateVC: self)
+        view.addSubview(collectionView)
+    }
 }
+
+extension AlbumDetailViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return album.photos?.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
+        let photos = album.photos?.allObjects as? [Photo]
+        let photo = photos?[indexPath.item]
+        cell.imageView.image = UIImage(data: (photo?.image)!)
+        return cell
+    }
+}
+
