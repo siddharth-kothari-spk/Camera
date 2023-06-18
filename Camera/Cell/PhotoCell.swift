884993728ff6151bcb278bc5b435e365f47c27d5
siddharth-kothari-spk <siddharth.kothari.spk@gmail.com>
2023-06-18 09:07:58 +0530

    added code for imageview collection cell for album detail and preview

--- Camera/Cell/PhotoCell.swift
+++ Camera/Cell/PhotoCell.swift
@@ -8,5 +8,19 @@
 import UIKit
 
 class PhotoCell: UICollectionViewCell {
+    let imageView: UIImageView
     
+    override init(frame: CGRect) {
+        imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: frame.width, height: frame.height))
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        
+        super.init(frame: frame)
+        
+        addSubview(imageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
